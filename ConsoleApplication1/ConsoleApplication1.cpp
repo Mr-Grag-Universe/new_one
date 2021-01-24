6e49c5fd0281980571e47bff7d69e9fff821b65e
sadadd <kafanovstepan@gmail.com>
2021-01-24 21:03:09 +0300

    start

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,16 @@
 ﻿#include <iostream>
+#include <string>
 
 int main()
 {
+	//the begining of work
 	std::cout << "Hello!" << std::endl;
+	std::string temp;
+	while (temp != "START") {
+	std::cout << "Input 'START' if you want to start work with calculator, and 'FINISH' otherwise.";
+	std::cin >> temp;
+	if (temp == "FINISH") { return 0; }
+	else if (temp != "START") { std::cout << "The command is not recognized. Please, repeat."; }
+	}
+	
 }
